4c98180c3d2c2a0fcb18cc0890572faa11348c20
sukidesu <hxysir@gmail.com>
2017-04-11 20:02:10 +0800

    3

--- app/src/main/java/com/example/sola/calculator/MainActivity.java
+++ app/src/main/java/com/example/sola/calculator/MainActivity.java
@@ -2,12 +2,110 @@ package com.example.sola.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btn_0;
+    Button btn_1;
+    Button btn_2;
+    Button btn_3;
+    Button btn_4;
+    Button btn_5;
+    Button btn_6;
+    Button btn_7;
+    Button btn_8;
+    Button btn_9;
+    Button btn_point;
+    Button btn_clear;
+    Button btn_del;
+    Button btn_plus;
+    Button btn_minus;
+    Button btn_multiply;
+    Button btn_divide;
+    Button btn_equle;
+    EditText et_input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_0= (Button) findViewById(R.id.btn_0);
+        btn_1= (Button) findViewById(R.id.btn_1);
+        btn_2= (Button) findViewById(R.id.btn_2);
+        btn_3= (Button) findViewById(R.id.btn_3);
+        btn_4= (Button) findViewById(R.id.btn_4);
+        btn_5= (Button) findViewById(R.id.btn_5);
+        btn_6= (Button) findViewById(R.id.btn_6);
+        btn_7= (Button) findViewById(R.id.btn_7);
+        btn_8= (Button) findViewById(R.id.btn_8);
+        btn_9= (Button) findViewById(R.id.btn_9);
+        btn_point= (Button) findViewById(R.id.btn_point);
+        btn_clear= (Button) findViewById(R.id.btn_clear);
+        btn_del= (Button) findViewById(R.id.btn_del);
+        btn_minus= (Button) findViewById(R.id.btn_minus);
+        btn_multiply= (Button) findViewById(R.id.btn_multiply);
+        btn_divide= (Button) findViewById(R.id.btn_divide);
+        btn_equle= (Button) findViewById(R.id.btn_equ);
+        btn_plus= (Button) findViewById(R.id.btn_plus);
+        et_input= (EditText)findViewById(R.id.et_input);
+
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_point.setOnClickListener(this);
+        btn_clear.setOnClickListener(this);
+        btn_del.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
+        btn_equle.setOnClickListener(this);
+        btn_plus.setOnClickListener(this);
+        et_input.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String str=et_input.getText().toString();
+        switch (v.getId()){
+            case R.id.btn_0:
+            case R.id.btn_1:
+            case R.id.btn_2:
+            case R.id.btn_3:
+            case R.id.btn_4:
+            case R.id.btn_5:
+            case R.id.btn_6:
+            case R.id.btn_7:
+            case R.id.btn_8:
+            case R.id.btn_9:
+            case R.id.btn_point:
+                et_input.setText(str+((Button)v).getText());
+                break;
+            case R.id.btn_minus:
+            case R.id.btn_multiply:
+            case R.id.btn_divide:
+            case R.id.btn_equ:
+            case R.id.btn_plus:
+                et_input.setText(str+" "+((Button)v).getText()+" ");
+            case R.id.btn_clear:
+            case R.id.btn_del:
+                //到这   慕课21-5 5分钟
+
+            default:
+                break;
+
+        }
+
     }
 }
